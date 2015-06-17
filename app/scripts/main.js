f1cb935fc89bd5fd2bc21849cadaf5617ca8e09e
Asa Toburen <atopettit@gmail.com>
2015-06-17 12:58:39 -0700

    mobile most

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,19 @@
+//Accordion for projects 
 
+$(document).ready(function() {
+    $("#work h3").click(function() {
+        //slide up all the link lists
+        $("#work ul").slideUp();
+        //slide down the link list below the h3 clicked - only if its closed
+        if (!$(this).next().is(":visible")) {
+            $(this).next().slideDown();
+        }
+    });
+});
+
+
+
+// Filter to sort projects
 
 var filterBtns = $('.btn').click(function() {
     var $el = $('.' + this.id).fadeIn(450);
