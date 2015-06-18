37fb9f7e2170785f2fce325533ded4f0ec9440ff
Asa Toburen <atopettit@gmail.com>
2015-06-17 17:35:47 -0700

    formatting/filter script

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,18 @@
 //Accordion for projects 
 
 $(document).ready(function() {
+
     $("#work h3").click(function() {
-        //slide up all the link lists
         $("#work ul").slideUp();
         //slide down the link list below the h3 clicked - only if its closed
         if (!$(this).next().is(":visible")) {
             $(this).next().slideDown();
         }
+    });
+    //when .btn is clicked slide up all other 
+    //btns
+    $(".btn").click(function() {
+        $(this).siblings().slideUp();
     });
 });
 
@@ -19,6 +24,5 @@ var filterBtns = $('.btn').click(function() {
     var $el = $('.' + this.id).fadeIn(450);
     $('#projects > article').not($el).hide();
     filterBtns.removeClass('active');
-    console.log(this);
     $(this).addClass('active');
 });
